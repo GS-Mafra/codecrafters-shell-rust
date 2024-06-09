36eb168b8873da00950cd9c519c5aaf26232bfdc
GS-Mafra <117572491+GS-Mafra@users.noreply.github.com>
2024-06-09 19:33:36 -0300

    echo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::io::{self, Write};
+use std::io::{self, Cursor, Write};
+
+use bytes::Buf;
 
 fn main() -> anyhow::Result<()> {
     loop {
@@ -7,12 +9,38 @@ fn main() -> anyhow::Result<()> {
 
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
-        let input = input.trim_end();
+        let input = input.trim();
 
-        match input {
-            "exit 0" => break,
-            _ => println!("{input}: command not found"),
+        let cur = &mut Cursor::new(input);
+        match until_space(cur) {
+            "exit" => {
+                // TODO
+                let _code = str_chunk(cur).parse::<u8>()?;
+                break;
+            }
+            "echo" => println!("{}", str_chunk(cur)),
+            cmd => println!("{cmd}: command not found"),
         }
     }
     Ok(())
+}
+
+fn until_space<'a>(cur: &mut Cursor<&'a str>) -> &'a str {
+    let chunk = cur.chunk();
+    let start = cur.get_ref().len() - chunk.len();
+    let pos = chunk.iter().position(u8::is_ascii_whitespace);
+
+    if let Some(pos) = pos {
+        cur.advance(pos + 1);
+        &cur.get_ref()[start..start + pos]
+    } else {
+        cur.advance(chunk.len());
+        cur.get_ref()
+    }
+}
+
+#[inline]
+fn str_chunk<'a>(cur: &'a Cursor<&'a str>) -> &'a str {
+    let chunk = cur.chunk();
+    unsafe { std::str::from_utf8_unchecked(chunk) }
 }
